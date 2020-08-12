4347c4d9be90beab0a4dd25ddc998ecf618cb3af
yuukiyokoyama <yuukitigars89@gmail.com>
2020-08-12 16:54:00 +0900

    Revert "Revert "’posts#index’のビュー作成"" branch
    This reverts commit c49d5f09f90ac2ddca93be0760c3cd7e8fca77d4.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'posts#index'
+  get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
 end
