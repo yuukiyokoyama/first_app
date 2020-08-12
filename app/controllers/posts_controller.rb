3cb13ceded1754e90288a708cd40f19b91c3f9ce
yuukiyokoyama <yuukitigars89@gmail.com>
2020-08-12 15:43:57 +0900

    postsコントローラー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,4 @@
 class PostsController < ApplicationController
-  def index  # indexアクションを定義した
-    @posts = Post.all  # すべてのレコードを@postsに代入
+  def index
   end
-  
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
-  end  
-end
+ end
